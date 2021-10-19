739fd3406d27b5b3196d2d5e1b795e57f6e16d95
Borislav <borislav.stefanov.1995@gmail.com>
2021-10-19 15:09:10 +0300

    Env added

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const { graphqlHTTP } = require("express-graphql");
 const mongoose = require("mongoose");
 const schema = require("./schema/schema");
 const isAuth = require("./middleware/Auth");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const url = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@demo-shard-00-00.s5bfb.mongodb.net:27017,demo-shard-00-01.s5bfb.mongodb.net:27017,demo-shard-00-02.s5bfb.mongodb.net:27017/${process.env.MONGO_DB}?ssl=true&replicaSet=atlas-y5tgea-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
@@ -27,15 +29,17 @@ app.use(
     graphiql: true,
   })
 );
+app.get("/", (req, res) => res.send("This is LUMINEXYS API!"));
 
 mongoose
   .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     app.listen(4000, () => {
+      console.log(process.env.MONGO_DB);
       console.log("listening");
     });
   })
   .catch((err) => {
-    console.log(process.env);
+    console.log(process.env.MONGO_DB);
     console.log(err);
   });
